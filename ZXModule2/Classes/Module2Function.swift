89491f459cbf22c08d36aad9a8234fde2e391797
paul.yin <paul.yin@yff.com>
2021-05-01 14:16:56 +0800

    添加hello

---
+++ ZXModule2/Classes/Module2Function.swift
@@ -0,0 +1,16 @@
+//
+//  Module2Function.swift
+//  Pods-ZXProjectDemo_Example
+//
+//  Created by ZXWin on 2021/5/1.
+//
+
+import Foundation
+
+public struct Module2Function {
+    
+    public static func hello() {
+        print("hello i am Module2")
+    }
+    
+}
